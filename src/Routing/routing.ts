1a81ac72f6cf20894d7158a35ee4e0444c7f850a
Shahwaiz24 <shahwaizafzal90@gmail.com>
2024-07-26 07:33:37 -0700

    Updated Code

--- src/Routing/routing.ts
+++ src/Routing/routing.ts
@@ -34,3 +34,29 @@ userRouting.delete('/deleteusers', (request: express.Request, response: express.
 })
 
 export default userRouting;
+
+userRouting.get('/login', (request: express.Request, response: express.Response) => {
+
+    let body = request.body;
+    // let jsonResponse = JSON.parse(body)
+    // response.status(200)
+    // response.send(`<h1>${jsonResponse}</h1>`)
+
+    if (request.statusCode == 200 && body.name == 'shahwaiz') {
+        response.status(200).json({
+            'name': body.name,
+            'email': body.email,
+            'msg': 'Login Success'
+        });
+
+    }
+    else {
+        response.status(400).json({
+            'name': body.name,
+            'email': body.email,
+            'msg': 'Invalid Email'
+        });
+    }
+
+
+})
--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,13 @@ import express from 'express';
 import userRouting from './Routing/routing';
 
 const app: express.Application = express()
+app.use(express.json());
 
 const hostName = 'localhost'
 const port = 5000
 
-app.use('/v1/api/users', userRouting)
+app.use('/v1/api/users', userRouting),
+   
 
 app.listen(port, hostName, () => {
     console.log(`http://${hostName}:${port}`)
